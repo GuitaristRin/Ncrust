d9a2cf811406dd520eaf4be3073ac68c50a66753
TakahashiRinta <1586182829@qq.com>
2026-08-04 11:00:04 +0800

    build: 升级至 v1.2.2 (versionCode 4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.takahashirinta.ncrust"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2.1"
+        versionCode = 4
+        versionName = "1.2.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
